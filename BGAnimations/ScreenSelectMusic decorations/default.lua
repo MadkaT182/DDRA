3c463e168e29caee84b57f61fc49475eb7da96d1
unknown <madcat182@gmail.com>
2016-04-05 23:25:17 -0500

    Added radar BG

--- BGAnimations/ScreenSelectMusic decorations/default.lua
+++ BGAnimations/ScreenSelectMusic decorations/default.lua
@@ -1,18 +1,46 @@
-return Def.ActorFrame {
+local t = Def.ActorFrame {};
+
+t[#t+1] = Def.ActorFrame {
 	LoadActor( "stagebg" )..{
-		InitCommand=cmd(x,SCREEN_LEFT+82;y,SCREEN_TOP+37;);
-	};
+		InitCommand=cmd(x,SCREEN_LEFT+82;y,SCREEN_TOP+37);
+	}
+};
 
+t[#t+1] = Def.ActorFrame {
 	LoadActor( "sidebg" )..{
-		InitCommand=cmd(x,SCREEN_LEFT+124;y,SCREEN_BOTTOM-215;rotationy,180;);
-	};
+		InitCommand=cmd(x,SCREEN_LEFT+124;y,SCREEN_BOTTOM-215;rotationy,180);
+	}
+};
 
+t[#t+1] = Def.ActorFrame {
 	LoadActor( "sidebg" )..{
-		InitCommand=cmd(x,SCREEN_RIGHT-124;y,SCREEN_BOTTOM-215;);
-	};
+		InitCommand=cmd(x,SCREEN_RIGHT-124;y,SCREEN_BOTTOM-215);
+	}
+};
 
+--RadarBG P1
+if GAMESTATE:IsPlayerEnabled(PLAYER_1) and GAMESTATE:GetPlayMode() ~= 'PlayMode_Oni' then
+	t[#t+1] = Def.ActorFrame {
+		LoadActor("radarbg")..{
+			OnCommand=cmd(x,SCREEN_LEFT+81;y,SCREEN_CENTER_Y+36);
+			OffCommand=cmd(diffusealpha,1);
+		}
+	};
+end
+
+--RadarBG P2
+if GAMESTATE:IsPlayerEnabled(PLAYER_2) and GAMESTATE:GetPlayMode() ~= 'PlayMode_Oni' then
+	t[#t+1] = Def.ActorFrame {
+		LoadActor("radarbg")..{
+			OnCommand=cmd(x,SCREEN_RIGHT-81;y,SCREEN_CENTER_Y+36);
+			OffCommand=cmd(diffusealpha,1);
+		}
+	};
+end
+
+t[#t+1] = Def.ActorFrame {
 	LoadActor( "../_footer" )..{
-		--InitCommand=cmd();
-	};
+	}
+};
 
-}
+return t
