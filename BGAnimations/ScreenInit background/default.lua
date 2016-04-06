91038571c72796d3f5a252a232119a41954b9828
unknown <madcat182@gmail.com>
2016-04-05 23:25:03 -0500

    Code cleaning

--- BGAnimations/ScreenInit background/default.lua
+++ BGAnimations/ScreenInit background/default.lua
@@ -1,6 +1,5 @@
 return Def.ActorFrame {
 	LoadActor( "warning" )..{
-		--InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtoheight,SCREEN_HEIGHT);
 	};
 }
