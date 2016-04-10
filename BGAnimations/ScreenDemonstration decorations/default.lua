7f72fabec2e260c2872f11855c507eac73bc4f2d
unknown <madcat182@gmail.com>
2016-04-10 14:53:19 -0500

    Added artbg on screen demosntration

--- BGAnimations/ScreenDemonstration decorations/default.lua
+++ BGAnimations/ScreenDemonstration decorations/default.lua
@@ -1,8 +1,8 @@
-local t = LoadFallbackB();
+local t = Def.ActorFrame {};
 t[#t+1] = LoadActor( THEME:GetPathB("_Arcade","decorations") );
 
 t[#t+1] = LoadActor("cdbg")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+	OnCommand=cmd(x,SCREEN_RIGHT-171;y,SCREEN_CENTER_Y+24;draworder,1);
 	OffCommand=cmd();
 };
 
