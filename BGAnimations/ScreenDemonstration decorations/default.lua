367de8e2182a19cc921cffd78cf98eb287a44362
unknown <madcat182@gmail.com>
2016-04-10 23:24:56 -0500

    Added demosntration overlay

--- BGAnimations/ScreenDemonstration decorations/default.lua
+++ BGAnimations/ScreenDemonstration decorations/default.lua
@@ -1,6 +1,11 @@
 local t = Def.ActorFrame {};
 t[#t+1] = LoadActor( THEME:GetPathB("_Arcade","decorations") );
 
+t[#t+1] = LoadActor("title")..{
+	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+23;draworder,1);
+	OffCommand=cmd();
+};
+
 t[#t+1] = LoadActor("cdbg")..{
 	OnCommand=cmd(x,SCREEN_RIGHT-171;y,SCREEN_CENTER_Y+24;draworder,1);
 	OffCommand=cmd();
