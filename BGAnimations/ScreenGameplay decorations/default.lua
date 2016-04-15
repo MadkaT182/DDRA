a0a834fbf79e66bcd03c1f0ae473a09b85130fe2
unknown <madcat182@gmail.com>
2016-04-15 11:03:27 -0500

    Added options indicators to gameplay

--- BGAnimations/ScreenGameplay decorations/default.lua
+++ BGAnimations/ScreenGameplay decorations/default.lua
@@ -2,6 +2,14 @@ local t = Def.ActorFrame{};
 
 t[#t+1] = StandardDecorationFromFile("SongInformation","SongInformation")
 
+t[#t+1] = LoadActor( THEME:GetPathB("","optionicon_P1") ) .. {
+	InitCommand=cmd(player,PLAYER_1;x,SCREEN_LEFT+108;y,SCREEN_CENTER_Y+165;draworder,1);
+};
+
+t[#t+1] = LoadActor( THEME:GetPathB("","optionicon_P2") ) .. {
+	InitCommand=cmd(player,PLAYER_2;x,SCREEN_LEFT+286;y,SCREEN_CENTER_Y+165;draworder,1);
+};
+
 t[#t+1] = StandardDecorationFromFile("ScoreFrame","ScoreFrame")
 
 return t
