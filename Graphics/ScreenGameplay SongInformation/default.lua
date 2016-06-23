a42ad9986d8451c56ce8cc3aed6de53c2747810d
unknown <madcat182@gmail.com>
2016-06-23 10:00:54 -0500

    Updated song information in screen gameplay

--- Graphics/ScreenGameplay SongInformation/default.lua
+++ Graphics/ScreenGameplay SongInformation/default.lua
@@ -5,21 +5,43 @@ t[#t+1] = LoadActor("SongNamebg")..{
 }
 
 --Song Information
-t[#t+1] = LoadFont("_arial black 20px") .. {
+t[#t+1] = LoadFont("_arial_black 18px") .. {
 	CurrentSongChangedMessageCommand=cmd(playcommand,"Refresh");
 	RefreshCommand=function(self)
 		local vSong = GAMESTATE:GetCurrentSong();
 		local vCourse = GAMESTATE:GetCurrentCourse();
 		local sText = ""
-		if vSong then
-			sText = vSong:GetDisplayFullTitle() .. "\n" .. vSong:GetDisplayArtist()
-		end
-		if vCourse then
-			sText = vSong:GetDisplayFullTitle() .. "\n" .. vSong:GetDisplayArtist()
-		end
+		-- if vSong then
+			sText = vSong:GetDisplayFullTitle()
+		-- end
+		-- if vCourse then
+		-- 	sText = vSong:GetDisplayFullTitle()
+		-- end
 		self:settext( sText );
 		self:horizalign(center);
 		self:playcommand( "On" );
+		self:addy(-7);
+		self:maxwidth(220);
+	end;
+}
+
+--Artist Information
+t[#t+1] = LoadFont("_arial_blk12px") .. {
+	CurrentSongChangedMessageCommand=cmd(playcommand,"Refresh");
+	RefreshCommand=function(self)
+		local vSong = GAMESTATE:GetCurrentSong();
+		local vCourse = GAMESTATE:GetCurrentCourse();
+		local sText = ""
+		-- if vSong then
+			sText = vSong:GetDisplayArtist()
+		-- end
+		-- if vCourse then
+		-- 	sText = vSong:GetDisplayArtist()
+		-- end
+		self:settext( sText );
+		self:horizalign(center);
+		self:playcommand( "On" );
+		self:addy(11);
 		self:maxwidth(220);
 	end;
 }
