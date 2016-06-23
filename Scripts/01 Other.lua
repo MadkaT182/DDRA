f1be5f566f5ada0962887ca54e243c798fde9165
unknown <madcat182@gmail.com>
2016-06-23 10:06:43 -0500

    Added script for stage counter

--- Scripts/01 Other.lua
+++ Scripts/01 Other.lua
@@ -21,3 +21,43 @@ function LoadStepsDisplayGameplayFrame(self,player)
 	if player == PLAYER_2 then state = state + 1; end;
 	return state;
 end;
+
+local numbered_stages= {
+	Stage_1st= true,
+	Stage_2nd= true,
+	Stage_3rd= true,
+	Stage_4th= true,
+	Stage_5th= true,
+	Stage_6th= true,
+	Stage_Next= true,
+}
+
+function thified_curstage_index(on_eval)
+	local cur_stage= GAMESTATE:GetCurrentStage()
+	local adjust= 1
+	-- hack: ScreenEvaluation shows the current stage, but it needs to show
+	-- the last stage instead.  Adjust the amount.
+	if on_eval then
+		adjust= 0
+	end
+
+	local ordinal = "th";
+
+	if GAMESTATE:GetCurrentStageIndex() + adjust == 1 then
+		ordinal ="st";
+	elseif GAMESTATE:GetCurrentStageIndex() + adjust == 2 then
+		ordinal ="nd";
+	elseif GAMESTATE:GetCurrentStageIndex() + adjust == 3 then
+		ordinal ="rd";
+	end
+
+	if GAMESTATE:GetCurrentStage() == "Stage_Final" or GAMESTATE:IsExtraStage() or GAMESTATE:IsExtraStage2() then
+		ordinal = "";
+	end
+
+	if numbered_stages[cur_stage] then
+		return GAMESTATE:GetCurrentStageIndex() + adjust .. ordinal
+	else
+		return ToEnumShortString(cur_stage) .. ordinal
+	end
+end
