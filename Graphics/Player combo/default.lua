15cc98a84f6b8a8ac25d64e27e3df501a1e1ecda
unknown <madcat182@gmail.com>
2016-09-01 11:17:20 -0500

    Updated combo graphics

--- Graphics/Player combo/default.lua
+++ Graphics/Player combo/default.lua
@@ -1,10 +1,11 @@
 local c;
 local cf;
-local canAnimate = false;
 local player = Var "Player";
 local ShowComboAt = THEME:GetMetric("Combo", "ShowComboAt");
 local Pulse = THEME:GetMetric("Combo", "PulseCommand");
 local PulseLabel = THEME:GetMetric("Combo", "PulseLabelCommand");
+local LowerJudge={0,0};
+local p=((player=='PlayerNumber_P1') and 1 or 2);
 
 local NumberMinZoom = THEME:GetMetric("Combo", "NumberMinZoom");
 local NumberMaxZoom = THEME:GetMetric("Combo", "NumberMaxZoom");
@@ -13,77 +14,89 @@ local NumberMaxZoomAt = THEME:GetMetric("Combo", "NumberMaxZoomAt");
 local LabelMinZoom = THEME:GetMetric("Combo", "LabelMinZoom");
 local LabelMaxZoom = THEME:GetMetric("Combo", "LabelMaxZoom");
 
---local ShowFlashyCombo = ThemePrefs.Get("FlashyCombo")
-
 local t = Def.ActorFrame {
-	InitCommand=cmd(vertalign,bottom);
-	-- flashy combo elements:
- -- 	LoadActor(THEME:GetPathG("Combo","100Milestone")) .. {
-	-- 	Name="OneHundredMilestone";
-	-- 	InitCommand=cmd(visible,ShowFlashyCombo);
-	-- 	FiftyMilestoneCommand=cmd(playcommand,"Milestone");
-	-- };
-	-- LoadActor(THEME:GetPathG("Combo","1000Milestone")) .. {
-	-- 	Name="OneThousandMilestone";
-	-- 	InitCommand=cmd(visible,ShowFlashyCombo);
-	-- 	ToastyAchievedMessageCommand=cmd(playcommand,"Milestone");
-	-- };
-	-- normal combo elements:
 	Def.ActorFrame {
 		Name="ComboFrame";
-		LoadFont( "Combo", "numbers" ) .. {
-			Name="Number";
+		LoadFont( "combo", "marv" ) .. {
+			Name="NumberW1";
 			OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
 		};
-		LoadActor("_combo")..{
-			Name="ComboLabel";
-			OnCommand = THEME:GetMetric("Combo", "ComboLabelOnCommand");
+		LoadFont( "combo", "perf" ) .. {
+			Name="NumberW2";
+			OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
+		};
+		LoadFont( "combo", "great" ) .. {
+			Name="NumberW3";
+			OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
+		};
+		LoadFont( "combo", "normal") .. {
+			Name="NumberNormal";
+			OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
+		};
+		LoadActor("_combomarv") .. {
+			Name="LabelW1";
+			OnCommand = THEME:GetMetric("Combo", "LabelOnCommand");
+		};
+		LoadActor("_comboperfect") .. {
+			Name="LabelW2";
+			OnCommand = THEME:GetMetric("Combo", "LabelOnCommand");
+		};
+		LoadActor("_combogreat") .. {
+			Name="LabelW3";
+			OnCommand = THEME:GetMetric("Combo", "LabelOnCommand");
+		};
+		LoadActor("_combonormal") .. {
+			Name="LabelNormal";
+			OnCommand = THEME:GetMetric("Combo", "LabelOnCommand");
 		};
 	};
 	InitCommand = function(self)
 		c = self:GetChildren();
 		cf = c.ComboFrame:GetChildren();
-		cf.Number:visible(false);
-		cf.ComboLabel:visible(false)
+		cf.NumberW1:visible(false);
+		cf.NumberW2:visible(false);
+		cf.NumberW3:visible(false);
+		cf.NumberNormal:visible(false);
+		cf.LabelW1:visible(false);
+		cf.LabelW2:visible(false);
+		cf.LabelW3:visible(false);
+		cf.LabelNormal:visible(false);
+		LowerJudge[p]=0;
 	end;
-	-- Milestones:
-	-- 25,50,100,250,600 Multiples;
---[[ 		if (iCombo % 100) == 0 then
-			c.OneHundredMilestone:playcommand("Milestone");
-		elseif (iCombo % 250) == 0 then
-			-- It should really be 1000 but thats slightly unattainable, since
-			-- combo doesnt save over now.
-			c.OneThousandMilestone:playcommand("Milestone");
-		else
-			return
-		end; --]]
- 	TwentyFiveMilestoneCommand=function(self,parent)
-		-- if ShowFlashyCombo then
-		-- 	(cmd(finishtweening;addy,-4;bounceend,0.125;addy,4))(self);
-		-- end;
-	end;
-	--]]
-
-	ToastyAchievedMessageCommand=function(self,params)
-		if params.PlayerNumber == player then
-			(cmd(thump,2;effectclock,'beat'))(c.ComboFrame);
+	JudgmentMessageCommand = function(self,params)
+		if params.Player ~= player then return end;
+		if params.TapNoteScore=='TapNoteScore_W1' and LowerJudge[p]<1 then
+			LowerJudge[p]=1;
+		elseif params.TapNoteScore=='TapNoteScore_W2' and LowerJudge[p]<2 then
+			LowerJudge[p]=2;
+		elseif params.TapNoteScore=='TapNoteScore_W3' and LowerJudge[p]<3 then
+			LowerJudge[p]=3;
+		elseif params.TapNoteScore=='TapNoteScore_W4' and LowerJudge[p]<4 then
+			LowerJudge[p]=4;
+		elseif params.TapNoteScore=='TapNoteScore_CheckpointMiss' 
+			or params.TapNoteScore=='TapNoteScore_W5' 
+			or params.TapNoteScore=='TapNoteScore_Miss' then
+			LowerJudge[p]=1;
 		end;
 	end;
-	--[[
-	ToastyDroppedMessageCommand=function(self,params)
-		if params.PlayerNumber == player then
-			(cmd(stopeffect))(c.ComboFrame);
-		end;
-	end; --]]
 	ComboCommand=function(self, param)
-		local iCombo = param.Combo;
+		local iCombo = param.Misses or param.Combo;
 		if not iCombo or iCombo < ShowComboAt then
-			cf.Number:visible(false);
-			cf.ComboLabel:visible(false)
+			cf.NumberW1:visible(false);
+			cf.NumberW2:visible(false);
+			cf.NumberW3:visible(false);
+			cf.NumberNormal:visible(false);
+			cf.LabelW1:visible(false);
+			cf.LabelW2:visible(false);
+			cf.LabelW3:visible(false);
+			cf.LabelNormal:visible(false);
 			return;
 		end
 
-		cf.ComboLabel:visible(false)
+		cf.LabelW1:visible(false);
+		cf.LabelW2:visible(false);
+		cf.LabelW3:visible(false);
+		cf.LabelNormal:visible(true);
 
 		param.Zoom = scale( iCombo, 0, NumberMaxZoomAt, NumberMinZoom, NumberMaxZoom );
 		param.Zoom = clamp( param.Zoom, NumberMinZoom, NumberMaxZoom );
@@ -91,58 +104,72 @@ local t = Def.ActorFrame {
 		param.LabelZoom = scale( iCombo, 0, NumberMaxZoomAt, LabelMinZoom, LabelMaxZoom );
 		param.LabelZoom = clamp( param.LabelZoom, LabelMinZoom, LabelMaxZoom );
 
-		if param.Combo then
-			cf.ComboLabel:visible(true)
-		else
-			cf.ComboLabel:visible(false)
-		end
+		cf.NumberW1:settext( string.format("%i", iCombo) );
+		cf.NumberW2:settext( string.format("%i", iCombo) );
+		cf.NumberW3:settext( string.format("%i", iCombo) );
+		cf.NumberNormal:settext( string.format("%i", iCombo) );
+		if LowerJudge[p]==1 then
+			cf.NumberW1:visible(true);
+			cf.NumberW2:visible(false);
+			cf.NumberW3:visible(false);
+			cf.NumberNormal:visible(false);
 
-		cf.Number:visible(true);
-		cf.Number:settext( string.format("%i", iCombo) );
-        cf.Number:textglowmode("TextGlowMode_Stroke");
-		-- FullCombo Rewards
-		if param.FullComboW1 then
-			cf.Number:diffuse( GameColor.Judgment["JudgmentLine_W1"] );
-			cf.Number:strokecolor( GameColor.Judgment["JudgmentLine_W1"] );
-            cf.Number:textglowmode("TextGlowMode_Stroke");
-			cf.Number:glowshift();
-		elseif param.FullComboW2 then
-			cf.Number:diffuse( GameColor.Judgment["JudgmentLine_W2"] );
-			cf.Number:strokecolor( GameColor.Judgment["JudgmentLine_W2"] );
-            cf.Number:textglowmode("TextGlowMode_Stroke");
-			cf.Number:glowshift();
-		elseif param.FullComboW3 then
-			cf.Number:diffuse( GameColor.Judgment["JudgmentLine_W3"] );
-			cf.Number:strokecolor( GameColor.Judgment["JudgmentLine_W3"] );
-            cf.Number:textglowmode("TextGlowMode_Stroke");
-			cf.Number:glowshift();
-		elseif param.Combo then
-			-- Player 1's color is Red, which conflicts with the miss combo.
-			-- instead, just diffuse to white for now. -aj
-			--c.Number:diffuse(PlayerColor(player));
-			cf.Number:diffuse(Color("White"));
-			cf.Number:strokecolor(Color("Stealth"));
-			cf.Number:stopeffect();
+			cf.LabelW1:visible(true);
+			cf.LabelW2:visible(false);
+			cf.LabelW3:visible(false);
+			cf.LabelNormal:visible(false);
+		elseif LowerJudge[p]==2 then
+			cf.NumberW1:visible(false);
+			cf.NumberW2:visible(true);
+			cf.NumberW3:visible(false);
+			cf.NumberNormal:visible(false);
+
+			cf.LabelW1:visible(false);
+			cf.LabelW2:visible(true);
+			cf.LabelW3:visible(false);
+			cf.LabelNormal:visible(false);
+		elseif LowerJudge[p]==3 then
+			cf.NumberW1:visible(false);
+			cf.NumberW2:visible(false);
+			cf.NumberW3:visible(true);
+			cf.NumberNormal:visible(false);
+
+			cf.LabelW1:visible(false);
+			cf.LabelW2:visible(false);
+			cf.LabelW3:visible(true);
+			cf.LabelNormal:visible(false);
+		elseif LowerJudge[p]==4 then
+			cf.NumberW1:visible(false);
+			cf.NumberW2:visible(false);
+			cf.NumberW3:visible(false);
+			cf.NumberNormal:visible(true);
+
+			cf.LabelW1:visible(false);
+			cf.LabelW2:visible(false);
+			cf.LabelW3:visible(false);
+			cf.LabelNormal:visible(true);
 		else
-			cf.Number:diffuse(color("#ff0000"));
-			cf.Number:stopeffect();
+			cf.NumberW1:visible(false);
+			cf.NumberW2:visible(false);
+			cf.NumberW3:visible(false);
+			cf.NumberNormal:visible(true);
+
+			cf.LabelW1:visible(false);
+			cf.LabelW2:visible(false);
+			cf.LabelW3:visible(false);
+			cf.LabelNormal:visible(true);
 		end
 		-- Pulse
-		Pulse( cf.Number, param );
-		if param.Combo then
-			PulseLabel( cf.ComboLabel, param );
-		end
+		Pulse( cf.NumberW1, param );
+		Pulse( cf.NumberW2, param );
+		Pulse( cf.NumberW3, param );
+		Pulse( cf.NumberNormal, param );
+		PulseLabel( cf.LabelW1, param );
+		PulseLabel( cf.LabelW2, param );
+		PulseLabel( cf.LabelW3, param );
+		PulseLabel( cf.LabelNormal, param );
 		-- Milestone Logic
 	end;
---[[ 	ScoreChangedMessageCommand=function(self,param)
-		local iToastyCombo = param.ToastyCombo;
-		if iToastyCombo and (iToastyCombo > 0) then
--- 			(cmd(thump;effectmagnitude,1,1.2,1;effectclock,'beat'))(c.Number)
--- 			(cmd(thump;effectmagnitude,1,1.2,1;effectclock,'beat'))(c.Number)
-		else
--- 			c.Number:stopeffect();
-		end;
-	end; --]]
 };
 
 return t;
