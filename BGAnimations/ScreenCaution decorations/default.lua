c7c348ad856a55b231d6a9a7e30a1ad2fa896dad
unknown <madcat182@gmail.com>
2016-09-01 11:09:29 -0500

    Screen caution timmings

--- BGAnimations/ScreenCaution decorations/default.lua
+++ BGAnimations/ScreenCaution decorations/default.lua
@@ -3,50 +3,56 @@ local t = Def.ActorFrame {};
 --Bg (placeholder)
 t[#t+1] = LoadActor("../_bluebg")..{};
 
---Top
-t[#t+1] = LoadActor("top")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-145);
-	OffCommand=cmd();
-};
 
---Top glow
-t[#t+1] = LoadActor("top")..{
-	OnCommand=cmd(queuecommand,"Animate";queuecommand,"Later");
-	AnimateCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-145;zoomy,1;diffusealpha,.5;linear,1;zoomy,1.53;linear,.5;diffusealpha,0;queuecommand,"Later");
-	LaterCommand=cmd(queuecommand, "Animate");
-	OffCommand=cmd();
-};
-
---Bottom
-t[#t+1] = LoadActor("bottom")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+139);
-	OffCommand=cmd();
-};
-
---Bottom glow
-t[#t+1] = LoadActor("bottom")..{
-	OnCommand=cmd(queuecommand,"Animate";queuecommand,"Later");
-	AnimateCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+139;zoomy,1;diffusealpha,.5;linear,1;zoomy,1.53;linear,.5;diffusealpha,0;queuecommand,"Later");
-	LaterCommand=cmd(queuecommand, "Animate");
-	OffCommand=cmd();
+--BGMiddle
+t[#t+1] = LoadActor("bgmid")..{
+	OnCommand=cmd(diffusealpha,0;Center;zoomy,0;sleep,0.8;diffusealpha,1;linear,0.066;zoom,1;linear,0.067;diffusealpha,0);
 };
 
 --Middle
 t[#t+1] = LoadActor("middle")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-3);
-	OffCommand=cmd();
+	OnCommand=cmd(diffusealpha,0;Center;zoomy,0;sleep,0.8;diffusealpha,1;linear,0.066;zoom,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-3);
+	OffCommand=cmd(linear,0.134;zoomy,0);
 };
 
 --Text
 t[#t+1] = LoadActor("text")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
-	OffCommand=cmd();
+	OnCommand=cmd(diffusealpha,0;Center;zoomy,0;sleep,0.8;diffusealpha,1;linear,0.066;zoom,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+	OffCommand=cmd(linear,0.134;zoomy,0);
+};
+
+--Top
+t[#t+1] = LoadActor("top")..{
+	OnCommand=cmd(diffusealpha,0;zoomx,1.75;zoomy,0.75;sleep,0.734;diffusealpha,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-10;linear,0.066;zoom,1;addy,-4;linear,0.084;y,SCREEN_CENTER_Y-145);
+	OffCommand=cmd(linear,0.084;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-10;sleep,0.05;zoomx,1.75;zoomy,0.75;diffusealpha,0;);
+};
+
+--Top glow
+t[#t+1] = LoadActor("top")..{
+	OnCommand=cmd(diffusealpha,0;sleep,0.884;diffusealpha,1;playcommand,"Animate");--queuecommand,"Animate";queuecommand,"Later");
+	AnimateCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-145;zoomy,1;diffusealpha,.5;linear,1;zoomy,1.53;linear,.5;diffusealpha,0;queuecommand,"Later");
+	LaterCommand=cmd(queuecommand, "Animate");
+	OffCommand=cmd(stoptweening;diffusealpha,0);
+};
+
+--Bottom
+t[#t+1] = LoadActor("bottom")..{
+	OnCommand=cmd(diffusealpha,0;zoomx,1.75;zoomy,0.75;sleep,0.734;diffusealpha,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+10;linear,0.066;zoom,1;addy,4;linear,0.084;y,SCREEN_CENTER_Y+139);
+	OffCommand=cmd(linear,0.084;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+10;sleep,0.05;zoomx,1.75;zoomy,0.75;diffusealpha,0);
+};
+
+--Bottom glow
+t[#t+1] = LoadActor("bottom")..{
+	OnCommand=cmd(diffusealpha,0;sleep,0.884;diffusealpha,1;playcommand,"Animate");--queuecommand,"Animate";queuecommand,"Later");
+	AnimateCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+139;zoomy,1;diffusealpha,.5;linear,1;zoomy,1.53;linear,.5;diffusealpha,0;queuecommand,"Later");
+	LaterCommand=cmd(queuecommand, "Animate");
+	OffCommand=cmd(stoptweening;diffusealpha,0);
 };
 
 --Caution
 t[#t+1] = LoadActor("caution")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-143);
-	OffCommand=cmd();
+	OnCommand=cmd(diffusealpha,0;zoomx,1.75;zoomy,0.75;sleep,0.734;diffusealpha,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-8;linear,0.066;zoom,1;addy,-4;linear,0.084;y,SCREEN_CENTER_Y-143);
+	OffCommand=cmd(linear,0.084;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-8;sleep,0.5;zoomx,1.75;zoomy,0.75;diffusealpha,0;);
 };
 
 return t
