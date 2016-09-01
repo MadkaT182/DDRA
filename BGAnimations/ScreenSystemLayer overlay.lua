d445d470eeda31c1120251b91b1093647c692d58
unknown <madcat182@gmail.com>
2016-09-01 11:18:59 -0500

    Updated system layer overlay

--- BGAnimations/ScreenSystemLayer overlay.lua
+++ BGAnimations/ScreenSystemLayer overlay.lua
@@ -1,12 +1,26 @@
-local function CreditsText( pn )
-	local text = LoadFont(Var "LoadingScreen","credits") .. {
-		InitCommand=function(self)
-			self:name("Credits" .. PlayerNumberToString(pn))
-			ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen");
-		end;
-		UpdateTextCommand=function(self)
-			local str = ScreenSystemLayerHelpers.GetCreditsMessage(pn);
-			self:settext(str);
+local function CreditsText()
+	local text = LoadFont("_sys1") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X-27;y,SCREEN_BOTTOM-7;playcommand,"Refresh");
+		RefreshCommand=function(self)
+		--Other coin modes
+			if GAMESTATE:IsEventMode() then self:settext('EVENT MODE') return end
+			if GAMESTATE:GetCoinMode()=='CoinMode_Free' then self:settext('FREE PLAY') return end
+			if GAMESTATE:GetCoinMode()=='CoinMode_Home' then self:settext('') return end
+		--Normal pay
+			local coins=GAMESTATE:GetCoins()
+			local coinsPerCredit=PREFSMAN:GetPreference('CoinsPerCredit')
+			local credits=math.floor(coins/coinsPerCredit)
+			local remainder=math.mod(coins,coinsPerCredit)
+			local s='CREDIT:'
+			if credits > 1 then
+				s='CREDITS:'..credits
+			elseif credits == 1 then
+				s=s..credits
+			else
+				s=s..0
+			end
+			self:horizalign(left)
+			self:settext(s)
 		end;
 		UpdateVisibleCommand=function(self)
 			local screen = SCREENMAN:GetTopScreen();
@@ -14,36 +28,69 @@ local function CreditsText( pn )
 			if screen then
 				local sClass = screen:GetName();
 				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
-			end
+			end;
 
 			self:visible( bShow );
-		end
+		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 	};
 	return text;
 end;
 
-local netConnected = IsNetConnected();
-local loggedOnSMO = IsNetSMOnline();
+local function CoinsText()
+	local text = LoadFont("_sys1") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X+74;y,SCREEN_BOTTOM-7;playcommand,"Refresh");
+		RefreshCommand=function(self)
+			local coins=GAMESTATE:GetCoins()
+			local coinsPerCredit=PREFSMAN:GetPreference('CoinsPerCredit')
+			local remainder=math.mod(coins,coinsPerCredit)
+			local s='COIN:'
+			if coinsPerCredit > 1 then
+				s=s..remainder..'/'..coinsPerCredit
+			else
+				s=''
+			end
+			self:settext(s)
+		end;
+		UpdateVisibleCommand=function(self)
+			local screen = SCREENMAN:GetTopScreen();
+			local bShow = true;
+			if screen then
+				local sClass = screen:GetName();
+				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
+			end;
+
+			self:visible( bShow );
+		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+	};
+	return text;
+end;
 
 local function NetworkText()
-	-- body
-	local text = LoadFont("ScreenSystemLayer credits") .. {
+	local text = LoadFont("_sys1") .. {
 		InitCommand=function (self)
 			self:name("NetworkStatus");
-			self:settext("---");
-			self:x(SCREEN_CENTER_X);
-			self:y(SCREEN_BOTTOM-20);
+			self:settext("-----");
+			self:x(SCREEN_CENTER_X-217);
+			self:y(SCREEN_BOTTOM-7);
+			self:horizalign(left);
 		end;
-		UpdateTextCommand=function (self)
-			-- body
+		RefreshCommand=function (self)
+		local netConnected = IsNetConnected();
+		local loggedOnSMO = IsNetSMOnline();
 			if netConnected then
-				self:strokecolor(color("#00ffff"));
-				self:settext( Screen.String("Network OK") );
-				self:horizalign(right);
+				self:diffuse(color("#00E702"));
+				self:settext("ONLINE");
 			else
-				self:strokecolor(color("#000000"));
-				self:settext( Screen.String("Offline") );
-				self:horizalign(right);
+				self:diffuse(color("#FFFFFF"));
+				self:settext("OFFLINE");
 			end;
 		end;
 		UpdateVisibleCommand=function(self)
@@ -56,45 +103,22 @@ local function NetworkText()
 
 			self:visible( bShow );
 		end;
+		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 	};
 	return text;
 end;
 
 local t = Def.ActorFrame {}
 
-	--Network
 t[#t+1] = Def.ActorFrame {
- 	NetworkText();
-};
-
-	-- Credits
-t[#t+1] = Def.ActorFrame {
- 	CreditsText( PLAYER_1 );
-	CreditsText( PLAYER_2 );
-};
-	-- Text
-t[#t+1] = Def.ActorFrame {
-	Def.Quad {
-		InitCommand=cmd(zoomtowidth,SCREEN_WIDTH;zoomtoheight,30;horizalign,left;vertalign,top;y,SCREEN_TOP;diffuse,color("0,0,0,0"));
-		OnCommand=cmd(finishtweening;diffusealpha,0.85;);
-		OffCommand=cmd(sleep,3;linear,0.5;diffusealpha,0;);
-	};
-	Def.BitmapText{
-		Font="ScreenSystemLayer credits";
-		Name="Text";
-		InitCommand=cmd(maxwidth,750;horizalign,left;vertalign,top;y,SCREEN_TOP+10;x,SCREEN_LEFT+10;diffusealpha,0;);
-		OnCommand=cmd(finishtweening;diffusealpha,1;);
-		OffCommand=cmd(sleep,3;linear,0.5;diffusealpha,0;);
-	};
-	SystemMessageMessageCommand = function(self, params)
-		self:GetChild("Text"):settext( params.Message );
-		self:playcommand( "On" );
-		if params.NoAnimate then
-			self:finishtweening();
-		end
-		self:playcommand( "Off" );
-	end;
-	HideSystemMessageMessageCommand = cmd(finishtweening);
+ 	--CreditsText( PLAYER_1 );
+	--CreditsText( PLAYER_2 );
+	NetworkText();
+	CreditsText();
+	CoinsText();
 };
 
 return t;
