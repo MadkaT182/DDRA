8c04ccc7bfad0b7a120a65abf217553354d10bb3
unknown <madcat182@gmail.com>
2016-09-03 12:38:07 -0500

    Added fix for dancers bgvideos

--- BGAnimations/ScreenGameplay underlay/Random/default.lua
+++ BGAnimations/ScreenGameplay underlay/Random/default.lua
@@ -1,9 +1,10 @@
 local t = Def.ActorFrame{}
 
 local vidcount=1;
+local ext="";
 local num = math.random(0,vidcount);
 
-t[#t+1] = LoadActor( "Char"..num )..{
+t[#t+1] = LoadActor( "Char"..num..ext )..{
 InitCommand=cmd(draworder,1;Center;zoomto,SCREEN_WIDTH,SCREEN_HEIGHT;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffusealpha,1;decelerate,100;diffusealpha,1);
 };
 
