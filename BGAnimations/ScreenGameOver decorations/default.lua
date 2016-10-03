08e02bc5861d6f50de2a67c9d596215df3f94b43
unknown <madcat182@gmail.com>
2016-10-03 18:29:52 -0500

    Updated game over screen

--- BGAnimations/ScreenGameOver decorations/default.lua
+++ BGAnimations/ScreenGameOver decorations/default.lua
@@ -1,10 +1,23 @@
-local t = Def.ActorFrame{
+local t = Def.ActorFrame{}
 
-	LoadActor( "thanks" )..{
-		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoom,0.7);
+t[#t+1] = Def.ActorFrame {
+	LoadActor( "color" )..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;blend,'BlendMode_WeightedMultiply');
 	};
+	LoadActor( "thanks" )..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;blend,'BlendMode_NoEffect';zwrite,true;clearzbuffer,false;sleep,0.334;linear,4.933;zoom,0.791;);
+	};
+	LoadActor( "konami" )..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-137;blend,'BlendMode_NoEffect';zwrite,true;clearzbuffer,false;);
+	};
+	Def.Quad{
+		InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF");ztest,true);
+	};
+	Def.Quad{
+		InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF");diffusealpha,0;sleep,5.5;linear,0.233;diffusealpha,1;);
+	};
+};
 
-}
 
 t[#t+1] = Def.ActorFrame {
 	SOUND:PlayAnnouncer("game over")
