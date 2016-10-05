a2f67139655a3d87d10d6c2ba56ab2d8bf8581a6
Inorizushi <inorizushi@gmail.com>
2016-10-04 21:15:10 -0700

    TitleMenu fix

--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -1,24 +1,18 @@
 local t = Def.ActorFrame{};
 
 --Logo
+if GAMESTATE:GetCoinMode() == 'CoinMode_Home' then
 t[#t+1] = Def.ActorFrame {
 	LoadActor( "ScreenLogo decorations/ddra" )..{
 		InitCommand=cmd(x,SCREEN_CENTER_X-2;y,SCREEN_CENTER_Y-33;zoom,0.75);
-	}
-};
-
---Copyright
-t[#t+1] = Def.ActorFrame {
+	};
 	LoadActor( "ScreenLogo decorations/copy" )..{
 		InitCommand=cmd(x,SCREEN_CENTER_X-5;y,SCREEN_CENTER_Y+75;zoom,0.75);
-	}
-};
-
---Selector
-t[#t+1] = Def.ActorFrame{
+	};
 	LoadActor(THEME:GetPathB("","_Arcade Decorations/bg"))..{
 		InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_CENTER_Y+160;zoom,0.75);
 	};
 };
+end;
 
 return t
