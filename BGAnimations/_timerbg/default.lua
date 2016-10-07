73e958a0d97bca5a71e8f408bf184b3c439c3b2d
unknown <madcat182@gmail.com>
2016-10-07 16:46:56 -0500

    Added timer bganimation

--- BGAnimations/_timerbg/default.lua
+++ BGAnimations/_timerbg/default.lua
@@ -1,6 +1,19 @@
 return Def.ActorFrame {
+	InitCommand=function(self)
+		self:x(SCREEN_CENTER_X+288);
+		self:y(SCREEN_TOP+37);
+	end;
 	LoadActor( "timerbg" )..{
-		InitCommand=cmd(x,SCREEN_CENTER_X+288;y,SCREEN_TOP+37;);
 		OffCommand=cmd(linear,0.167;zoom,0);
+	};
+	LoadActor("timer_ov")..{
+		OnCommand=cmd(playcommand,"Tick");
+		TickCommand=cmd(diffusealpha,1;zoom,.8;linear,0.4;diffusealpha,0;sleep,0.6;queuecommand,"Tick");
+		OffCommand=cmd(stoptweening;linear,0.167;diffusealpha,0);
+	};
+	LoadActor("timer_ov")..{
+		OnCommand=cmd(playcommand,"Tick");
+		TickCommand=cmd(diffusealpha,1;zoom,.8;linear,0.4;zoom,1.2;diffusealpha,0;sleep,0.6;queuecommand,"Tick");
+		OffCommand=cmd(stoptweening;linear,0.167;diffusealpha,0);
 	};
 }
