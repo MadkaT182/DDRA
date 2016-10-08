829ae974bf997c9d22096bfe89516a2e3218c3ad
unknown <madcat182@gmail.com>
2016-10-07 23:24:12 -0500

    Updated size and position for full combo ring for p2

--- BGAnimations/ScreenEvaluationNormal decorations/grade/default.lua
+++ BGAnimations/ScreenEvaluationNormal decorations/grade/default.lua
@@ -269,7 +269,7 @@ local t = Def.ActorFrame {
 --P2--
 --FCRingP2--
 	LoadActor("GoodFullcombo_ring")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 		local pssp2 = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2")
 			if pssp2:FullComboOfScore('TapNoteScore_W4') then
@@ -279,7 +279,7 @@ local t = Def.ActorFrame {
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 		LoadActor("Fullcombo_ring")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 		local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if((staw =="StageAward_SingleDigitW3") or (staw =="StageAward_OneW3") or (staw =="StageAward_FullComboW3") ) then
@@ -289,7 +289,7 @@ local t = Def.ActorFrame {
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	LoadActor("PerfectFullcombo_ring")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 			local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if((staw =="StageAward_SingleDigitW2") or (staw =="StageAward_OneW2") or (staw =="StageAward_FullComboW2") ) then
@@ -299,7 +299,7 @@ local t = Def.ActorFrame {
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	LoadActor("MarvelousFullcombo_ring")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 			local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if(staw =="StageAward_FullComboW1") then
@@ -309,42 +309,42 @@ local t = Def.ActorFrame {
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	LoadActor("GoodFullcombo_lines")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 		local pssp1 = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2")
 			if pssp1:FullComboOfScore('TapNoteScore_W4') then
-				(cmd(linear,0.2;zoom,1;spin;effectmagnitude,0,0,-170))(self);
+				(cmd(linear,0.2;zoom,1.2;spin;effectmagnitude,0,0,-170))(self);
 			end;
 		end;
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	LoadActor("Fullcombo_lines")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 		local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if((staw =="StageAward_SingleDigitW3") or (staw =="StageAward_OneW3") or (staw =="StageAward_FullComboW3") ) then
-				(cmd(linear,0.2;zoom,1;spin;effectmagnitude,0,0,-170))(self);
+				(cmd(linear,0.2;zoom,1.2;spin;effectmagnitude,0,0,-170))(self);
 			end;
 		end;
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	
 	LoadActor("PerfectFullcombo_lines")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 			local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if((staw =="StageAward_SingleDigitW2") or (staw =="StageAward_OneW2") or (staw =="StageAward_FullComboW2") ) then
-				(cmd(linear,0.2;zoom,1;spin;effectmagnitude,0,0,-170))(self);
+				(cmd(linear,0.2;zoom,1.2;spin;effectmagnitude,0,0,-170))(self);
 			end;
 		end;
 		OffCommand=cmd(linear,0.2;zoom,0);
 	};
 	LoadActor("MarvelousFullcombo_lines")..{
-		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+400-90-10-10+45;y,SCREEN_CENTER_Y-30;zoom,0);
+		InitCommand=cmd(player,PLAYER_2;x,SCREEN_CENTER_X+239;y,SCREEN_CENTER_Y-30;zoom,0);
 		OnCommand=function(self)
 			local staw = STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetStageAward();
 			if(staw =="StageAward_FullComboW1") then
-				(cmd(linear,0.2;zoom,1;spin;effectmagnitude,0,0,-170))(self);
+				(cmd(linear,0.2;zoom,1.2;spin;effectmagnitude,0,0,-170))(self);
 			end;
 		end;
 		OffCommand=cmd(linear,0.2;zoom,0);
@@ -354,7 +354,7 @@ local t = Def.ActorFrame {
 --GradeP2--
 
 	LoadActor("Grade_AAA (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier01") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -364,7 +364,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_AAA (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier02") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -374,7 +374,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_AA1 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier03") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -384,7 +384,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_AA (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier04") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -394,7 +394,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_AA2 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier05") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -404,7 +404,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_A1 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier06") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -414,7 +414,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_A (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier07") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -424,7 +424,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_A2 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier08") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -434,7 +434,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_B1 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier09") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -444,7 +444,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_B (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier10") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -454,7 +454,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_B2 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier11") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -464,7 +464,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_C1 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier12") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -474,7 +474,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_C (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier13") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -484,7 +484,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_C2 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier14") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -494,7 +494,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_D1 (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier16") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -504,7 +504,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_D (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier15") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -514,7 +514,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_E (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Tier17") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
@@ -524,7 +524,7 @@ local t = Def.ActorFrame {
 	};
 
 	LoadActor("Grade_E (doubleres).png")..{
-		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+243;y,SCREEN_CENTER_Y-34;zoom,0);
+		InitCommand=cmd(player,PLAYER_2; x,SCREEN_CENTER_X+176;y,SCREEN_CENTER_Y-34;zoom,0);
 		OnCommand=function(self)
 			if(STATSMAN:GetCurStageStats():GetPlayerStageStats("PlayerNumber_P2"):GetGrade()=="Grade_Failed") then
 				(cmd(sleep,0.2;linear,0.2;zoom,0.675))(self);
