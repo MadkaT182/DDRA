50be22060c3758bb95057070b92d896427d2fd85
unknown <madcat182@gmail.com>
2016-12-02 12:15:04 -0500

    Removed danger display from Inori's screen filter

--- Graphics/NoteField board.lua
+++ Graphics/NoteField board.lua
@@ -3,32 +3,6 @@ local filter_color= color("0,0,0,1")
 local this_pn
 
 local args= {
-	--the danger display
-	Def.Quad{
-		InitCommand=function(self) self
-			:diffusealpha(0)
-			:diffuseshift()
-			:effectcolor1(color("1,0,0,0.8"))
-			:effectcolor2(color("1,0,0,0"))
-			:effectclock("music")
-			:fadeleft(1/32)
-			:faderight(1/32)
-			:hibernate(math.huge)
-		end,
-		PlayerStateSetCommand= function(self, param)
-			local pn= param.PlayerNumber
-			this_pn= pn
-			local style= GAMESTATE:GetCurrentStyle(pn)
-			local width= style:GetWidth(pn) + 32
-			self:setsize(width, _screen.h*4096):hibernate(0)
-		end,
-		HealthStateChangedMessageCommand= function(self, param)
-			if this_pn and param.PlayerNumber == this_pn then
-				self:linear(0.1)
-				:diffusealpha((param.HealthState == 'HealthState_Danger') and 1 or 0)
-			end
-		end
-	},
 	--the screen filter
 	Def.Quad{
 		InitCommand= function(self)
