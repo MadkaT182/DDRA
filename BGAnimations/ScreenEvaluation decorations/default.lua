8cce2106cb5ad1ab5f279e6478812155d9114eb9
unknown <madcat182@gmail.com>
2018-09-04 11:19:13 -0500

    Added profile names in evaluation

--- BGAnimations/ScreenEvaluation decorations/default.lua
+++ BGAnimations/ScreenEvaluation decorations/default.lua
@@ -1,28 +1,27 @@
-local t = Def.ActorFrame{};
-
---Header
-t[#t+1] = Def.ActorFrame {
-	LoadActor( "../_header" )..{
-		--InitCommand=cmd(x,SCREEN_RIGHT-53;y,SCREEN_TOP+61;);
-	}
+local t = Def.ActorFrame{
+	LoadActor("../_header");
 };
 
---Player Indicators
---P1
-if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+for player in ivalues(GAMESTATE:GetHumanPlayers()) do
 	t[#t+1] = Def.ActorFrame {
-		LoadActor( "../_common_graphics/p1result" )..{
-			InitCommand=cmd(x,SCREEN_LEFT+53;y,SCREEN_TOP+61;);
-		}
-	};
-end
-
---P2
-if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
-	t[#t+1] = Def.ActorFrame {
-		LoadActor( "../_common_graphics/p2result" )..{
-			InitCommand=cmd(x,SCREEN_RIGHT-53;y,SCREEN_TOP+61;);
-		}
+		LoadActor( "../_common_graphics/result_player" )..{
+			InitCommand=function(self)
+				self:x(player == PLAYER_1 and SCREEN_LEFT+53 or SCREEN_RIGHT-53);
+				self:y(SCREEN_TOP+61)
+				self:zoomx(player == PLAYER_1 and 1 or -1);
+			end;
+		};
+		LoadFont("_resPlayer")..{
+			InitCommand=function(self)
+				self:settext(GAMESTATE:GetPlayerDisplayName(player));
+				self:zoomx(1.18);
+				self:maxwidth(80);
+				self:horizalign(player == PLAYER_1 and right or left);
+				self:diffuse(color("#FFFF00"));
+				self:x(player == PLAYER_1 and SCREEN_LEFT+95 or SCREEN_RIGHT-95);
+				self:y(SCREEN_TOP+62)
+			end;
+		};
 	};
 end
 
